ed1370564e458ec9160b3f4d835792fe2a0bdba6
simina12 <souheir.yassmine@ensam-casa.ma>
2023-02-02 23:35:47 +0100

    flight khdam

--- src/main/java/com/ensamvol/repositories/CompanyRepository.java
+++ src/main/java/com/ensamvol/repositories/CompanyRepository.java
@@ -2,9 +2,7 @@ package com.ensamvol.repositories;
 
 import com.ensamvol.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CompanyRepository extends JpaRepository<Company,Long> {
     void deleteCompanyByIdCompany(Long idCompany);
 }
---
+++ src/main/java/com/ensamvol/web/VolController.java
@@ -0,0 +1,23 @@
+package com.ensamvol.web;
+
+import com.ensamvol.entities.Vol;
+import com.ensamvol.repositories.VolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+@Controller
+public class VolController {
+    @Autowired
+    private VolRepository volRepository;
+    @RequestMapping(value = "/Indexo")
+    public String Indexo(Model model){
+        List<Vol> vols=volRepository.findAll();
+        model.addAttribute("flights",vols);
+        return "flights";
+    }
+}
