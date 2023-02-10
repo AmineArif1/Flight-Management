7b629d4bac340fd7868c8dade72a16683c708713
simina12 <souheir.yassmine@ensam-casa.ma>
2023-02-11 00:22:14 +0100

    page tables

--- src/main/java/com/ensamvol/web/TableElements.java
+++ src/main/java/com/ensamvol/web/TableElements.java
@@ -1,17 +1,53 @@
 package com.ensamvol.web;
 
+import com.ensamvol.entities.*;
+import com.ensamvol.repositories.*;
+import com.ensamvol.service.CompanyService;
+import com.ensamvol.service.VolService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.datatransfer.ClipboardOwner;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "pages")
 public class TableElements {
+    @Autowired
+    private VolRepository volRepository;
+    private ClientRepository clientRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private CompanyRepository companyRepository;
+    @Autowired
+    private BilletRepository billetRepository;
+    private VolService volService;
+    private CompanyService companyService;
 
     @GetMapping(value = "table-elements")
-    public ModelMap mmTableElements() {
-        return new ModelMap();
+    public String table_element(ModelMap model) {
+        List<Vol> vols = volRepository.findAll();
+        model.addAttribute("flights", vols);
+        List<Employee> employees=employeeRepository.findAll();
+        model.addAttribute("employees",employees);
+        List<Company> companies =companyRepository.findAll();
+        model.addAttribute("companies",companies);
+        List<Billet> billets=billetRepository.findAll();
+        model.addAttribute("billets",billets);
+
+
+
+
+
+        return "pages/table-elements";
+
     }
+
+
+
 
 }
