2800735c5077d3bf4ef181c33f9e1eb346507ee8
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-13 00:37:31 +0100

    Update VolService.java

--- src/main/java/com/ensamvol/service/VolService.java
+++ src/main/java/com/ensamvol/service/VolService.java
@@ -1,5 +1,6 @@
 package com.ensamvol.service;
 
+import com.ensamvol.HelperClass.VolHelper;
 import com.ensamvol.entities.Ville;
 import com.ensamvol.entities.Vol;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,5 @@ public interface VolService {
 
     Ville getVilleDepart(Long idVol);
     Ville getVilleArrive(Long idVol);
-    void updateVol(Long idVol ,Vol vol);
-
+	public void updateVol(VolHelper updatedVol);
 }
