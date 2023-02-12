b45800a37593137af290fdbd26f8854928cceeb9
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-12 03:44:58 +0100

    Update TableElements.java

--- src/main/java/com/ensamvol/web/TableElements.java
+++ src/main/java/com/ensamvol/web/TableElements.java
@@ -3,6 +3,7 @@ package com.ensamvol.web;
 import com.ensamvol.entities.*;
 import com.ensamvol.repositories.*;
 import com.ensamvol.service.CompanyService;
+import com.ensamvol.service.VilleService;
 import com.ensamvol.service.VolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,30 +17,37 @@ import java.util.List;
 public class TableElements {
     @Autowired
     private VolRepository volRepository;
+    @Autowired
+
     private ClientRepository clientRepository;
+    @Autowired
+    private VilleService villeService;
     @Autowired
     private EmployeeRepository employeeRepository;
     @Autowired
     private CompanyRepository companyRepository;
     @Autowired
     private BilletRepository billetRepository;
+    @Autowired
+
     private VolService volService;
+    @Autowired
+
     private CompanyService companyService;
 
     @GetMapping(value = "table-elements")
     public String table_element(ModelMap model) {
         List<Vol> vols = volRepository.findAll();
         model.addAttribute("flights", vols);
-        List<Employee> employees=employeeRepository.findAll();
+        model.addAttribute("flight",volRepository.getById(40L));
+        model.addAttribute("villes",villeService.listVille());
+        model.addAttribute("display","none");
+       /*List<Employee> employees=employeeRepository.findAll();
         model.addAttribute("employees",employees);
         List<Company> companies =companyRepository.findAll();
         model.addAttribute("companies",companies);
         List<Billet> billets=billetRepository.findAll();
-        model.addAttribute("billets",billets);
-
-
-
-
+        model.addAttribute("billets",billets);*/
 
         return "pages/table-elements";
 
