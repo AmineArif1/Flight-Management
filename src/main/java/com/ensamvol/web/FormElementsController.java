b77e80994914dbcc5b3f8aec102e1c993493a701
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-12 03:45:19 +0100

    Update FormElementsController.java

--- src/main/java/com/ensamvol/web/FormElementsController.java
+++ src/main/java/com/ensamvol/web/FormElementsController.java
@@ -1,16 +1,22 @@
 package com.ensamvol.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ensamvol.service.VilleService;
+
 @Controller
 @RequestMapping(value = "pages")
 public class FormElementsController {
-
+	@Autowired
+	private VilleService villeService;
     @GetMapping(value = "form-elements")
-    public ModelMap mmFormElements() {
+    public ModelMap mmFormElements(ModelMap model) {
+    	model.addAttribute("villes",villeService.listVille());
+
         return new ModelMap();
     }
 
