0cf94f32073b548a79d859fb8dc86b2c3aa38843
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-14 16:59:15 +0100

    Create EmployeeHelper.java

---
+++ src/main/java/com/ensamvol/HelperClass/EmployeeHelper.java
@@ -0,0 +1,63 @@
+package com.ensamvol.HelperClass;
+
+public class EmployeeHelper {
+
+		public Integer idEmployee;
+	 	private Integer CIN;
+	    private String nom;
+	    private String prenom;
+	    private String dateNaissance;
+	    private String username;
+	    private String password;
+	    private String roleName;
+		public Integer getIdEmployee() {
+			return idEmployee;
+		}
+		public void setIdEmployee(Integer idEmployee) {
+			this.idEmployee = idEmployee;
+		}
+		public Integer getCIN() {
+			return CIN;
+		}
+		public void setCIN(Integer cIN) {
+			CIN = cIN;
+		}
+		public String getNom() {
+			return nom;
+		}
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+		public String getPrenom() {
+			return prenom;
+		}
+		public void setPrenom(String prenom) {
+			this.prenom = prenom;
+		}
+		public String getDateNaissance() {
+			return dateNaissance;
+		}
+		public void setDateNaissance(String dateNaissance) {
+			this.dateNaissance = dateNaissance;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getRoleName() {
+			return roleName;
+		}
+		public void setRoleName(String roleName) {
+			roleName = roleName;
+		}
+		
+	    
+}
