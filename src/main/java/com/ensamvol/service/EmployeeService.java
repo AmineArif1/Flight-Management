410b5cac93f7e096280306103549c3a70d7c5c8b
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-14 17:14:11 +0100

    Update EmployeeService.java

--- src/main/java/com/ensamvol/service/EmployeeService.java
+++ src/main/java/com/ensamvol/service/EmployeeService.java
@@ -1,39 +1,19 @@
 package com.ensamvol.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import com.ensamvol.HelperClass.EmployeeHelper;
+import com.ensamvol.entities.Employee;
+import com.ensamvol.entities.Personne;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.context.IContext;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
-public class EmailServiceImp implements EmailService {
-    private final JavaMailSender javaMailSender;
-    private final TemplateEngine templateEngine;
-
-    @Autowired
-    public EmailServiceImp(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
-        this.javaMailSender = javaMailSender;
-        this.templateEngine = templateEngine;
-    }
-
-    public void sendEmail(String to, String subject, String templateName, Context context) {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(to);
-            helper.setSubject(subject);
-            String htmlContent = templateEngine.process(templateName, (IContext) context);
-            helper.setText(htmlContent, true);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-        javaMailSender.send(message);
-    }
+public interface EmployeeService {
+    void addNewEmployee(Employee employee);
+    List<Employee> listEmployee();
+    void removeEmployee(Integer idEmployee);
+    void saveEmployee(Employee employee);
+   Personne loadUserEmployee(int id);
+   void updateEmployee(EmployeeHelper employeeHelper);
 }
