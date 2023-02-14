5d18a30c661584f1eb3434655ede9d11ac8ce773
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-14 16:57:09 +0100

    Update EmployeeImp.java

--- src/main/java/com/ensamvol/service/EmployeeImp.java
+++ src/main/java/com/ensamvol/service/EmployeeImp.java
@@ -1,23 +1,36 @@
 package com.ensamvol.service;
 
+import com.ensamvol.HelperClass.EmployeeHelper;
 import com.ensamvol.entities.Employee;
 import com.ensamvol.entities.Personne;
+import com.ensamvol.entities.Role;
+import com.ensamvol.repositories.AppRoleRepository;
+import com.ensamvol.repositories.AppUserRepository;
 import com.ensamvol.repositories.EmployeeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
 @Service
 @Transactional
 public class EmployeeImp implements EmployeeService {
-    final EmployeeRepository employeeRepository;
-    final AccountService accountService;
+	@Autowired
+    private  EmployeeRepository employeeRepository;
+	@Autowired
 
-    public  EmployeeImp(EmployeeRepository employeeRepository, AccountService accountService) {
-        this.employeeRepository = employeeRepository;
-        this.accountService = accountService;
-    }
+    private AccountService accountService;
+	@Autowired
+
+    private AppUserRepository appUserRepository;
+	@Autowired
+
+    private  AppRoleRepository appRoleRepository;
+
+ 
 
 
 
@@ -49,6 +62,30 @@ public class EmployeeImp implements EmployeeService {
     public Personne loadUserEmployee(int idEmployee) {
         return accountService.showUserById(idEmployee);
     }
+
+
+
+	@Override
+	public void updateEmployee(EmployeeHelper employeeHelper) {
+		Employee oldEmployee = employeeRepository.findByIdEmployee(employeeHelper.idEmployee);
+		int CIN=employeeHelper.getCIN();
+		String nom = employeeHelper.getNom();
+		String prenom = employeeHelper.getPrenom();
+		String dateNaissance = employeeHelper.getDateNaissance();
+		String username = employeeHelper.getUsername();
+		String password = employeeHelper.getPassword();
+		Role role= appRoleRepository.findByRoleName(employeeHelper.getRoleName());
+		List<Role> roles=new ArrayList<>();
+		roles.add(role);
+		oldEmployee.personne.setCIN(CIN);
+		oldEmployee.personne.setNom(nom);
+		oldEmployee.personne.setPrenom(prenom);
+		oldEmployee.personne.setDateNaissance(dateNaissance);
+		oldEmployee.personne.setUsername(username);
+		oldEmployee.personne.setPassword(password);
+		oldEmployee.personne.setRoles(roles);
+		employeeRepository.save(oldEmployee);
+	}
 
 
 
