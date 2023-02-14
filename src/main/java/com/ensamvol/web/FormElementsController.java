49b7791c6d188816c8ed9201d29e4848a4de42c7
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-14 16:54:38 +0100

    Update FormElementsController.java

--- src/main/java/com/ensamvol/web/FormElementsController.java
+++ src/main/java/com/ensamvol/web/FormElementsController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ensamvol.repositories.AppRoleRepository;
+import com.ensamvol.service.EmployeeService;
 import com.ensamvol.service.VilleService;
 
 @Controller
@@ -13,11 +16,16 @@ import com.ensamvol.service.VilleService;
 public class FormElementsController {
 	@Autowired
 	private VilleService villeService;
+	@Autowired
+	private AppRoleRepository appRoleRepository;
+	@Autowired 
+	private EmployeeService employeeService;
+	
     @GetMapping(value = "form-elements")
     public ModelMap mmFormElements(ModelMap model) {
     	model.addAttribute("villes",villeService.listVille());
-
+    	model.addAttribute("roles",appRoleRepository.findAll());
         return new ModelMap();
     }
-
+  
 }
