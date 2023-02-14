03b5605ac4a622a24425bb749a8596eaeda6e4bc
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-14 16:56:04 +0100

    Update CompanyRepository.java

--- src/main/java/com/ensamvol/repositories/CompanyRepository.java
+++ src/main/java/com/ensamvol/repositories/CompanyRepository.java
@@ -2,7 +2,8 @@ package com.ensamvol.repositories;
 
 import com.ensamvol.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company,Long> {
-    void deleteCompanyByIdCompany(Long idCompany);
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CompanyRepository extends JpaRepository<Company,Integer> {
+    void deleteCompanyByIdCompany(Integer idCompany);
 }
