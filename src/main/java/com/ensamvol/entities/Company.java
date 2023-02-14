b3c2508c315dc2fa2538c7c92e933ce28e0bf7db
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-14 16:59:48 +0100

    Update Company.java

--- src/main/java/com/ensamvol/entities/Company.java
+++ src/main/java/com/ensamvol/entities/Company.java
@@ -21,6 +21,34 @@ public class Company {
     @OneToMany(mappedBy = "company")
     private Collection<Avion> avion;
 
+	public Company() {
+		super();
+	}
+	public Company(Integer idCompany, String companyName, Collection<Avion> avion) {
+		super();
+		this.idCompany = idCompany;
+		this.companyName = companyName;
+		this.avion = avion;
+	}
+	public Integer getIdCompany() {
+		return idCompany;
+	}
+	public void setIdCompany(Integer idCompany) {
+		this.idCompany = idCompany;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public Collection<Avion> getAvion() {
+		return avion;
+	}
+	public void setAvion(Collection<Avion> avion) {
+		this.avion = avion;
+	}
+
 
 
 
