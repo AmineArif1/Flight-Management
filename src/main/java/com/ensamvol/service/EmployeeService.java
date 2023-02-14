04092276da27642254e88325889d8cac55490376
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-02-14 16:57:29 +0100

    Update EmployeeService.java

--- src/main/java/com/ensamvol/service/EmployeeService.java
+++ src/main/java/com/ensamvol/service/EmployeeService.java
@@ -1,17 +1,39 @@
 package com.ensamvol.service;
 
-import com.ensamvol.entities.Employee;
-import com.ensamvol.entities.Personne;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.IContext;
 
-import javax.transaction.Transactional;
-import java.util.List;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 
 @Service
-public interface EmployeeService {
-    void addNewEmployee(Employee employee);
-    List<Employee> listEmployee();
-    void removeEmployee(Integer idEmployee);
-    void saveEmployee(Employee employee);
-    Personne loadUserEmployee(int id);
+public class EmailServiceImp implements EmailService {
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
+    @Autowired
+    public EmailServiceImp(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
+        this.javaMailSender = javaMailSender;
+        this.templateEngine = templateEngine;
+    }
+
+    public void sendEmail(String to, String subject, String templateName, Context context) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            String htmlContent = templateEngine.process(templateName, (IContext) context);
+            helper.setText(htmlContent, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        javaMailSender.send(message);
+    }
 }
